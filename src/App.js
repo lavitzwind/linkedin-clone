dca78ce227141a64cbbaeec233e6915d04a943f9
milleseseau <leojimlavi@gmail.com>
2021-09-16 07:18:09 -0500

    header and home components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
+import Header from "./components/Header";
+import Home from "./components/Home";
 import Login from "./components/Login";
 
 function App() {
@@ -7,8 +9,12 @@ function App() {
 		<div className="App">
 			<Router>
 				<Switch>
-					<Route exact-path="/">
+					<Route exact path="/">
 						<Login />
+					</Route>
+					<Route path="/home">
+						<Header />
+						<Home />
 					</Route>
 				</Switch>
 			</Router>
