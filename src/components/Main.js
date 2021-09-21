0c4bc5a65f911ab9839161d3456d2b9edf4e7fe4
milleseseau <leojimlavi@gmail.com>
2021-09-21 07:09:30 -0500

    main, leftside, rightside components

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,28 @@
 import styled from "styled-components";
 
-const Main = () => {
-	return <Container>Main</Container>;
+const Main = (props) => {
+	return (
+		<Container>
+			<ShareBox>Share</ShareBox>
+		</Container>
+	);
 };
 
 const Container = styled.div`
 	grid-area: main;
 `;
+
+const CommonCard = styled.div`
+	text-align: center;
+	overflow: hidden;
+	margin-bottom: 8px;
+	background-color: #fff;
+	border-radius: 5px;
+	position: relative;
+	border: none;
+	box-shadow: 0 0 0 1px rgb(0 0 0 / 15%), 0 0 0 rgb(0 0 0 / 20%);
+`;
+
+const ShareBox = styled(CommonCard)``;
 
 export default Main;
